49cf5a6cf46d02409b07e9d876d98ba87bab9f11
Daniel Pointecker <daniel.pointecker@gmail.com>
2026-09-21 08:46:40 +0200

    add fonts

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,12 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $page->title()->esc() ?></title>
+  <?= css('assets/css/style.css') ?>
+</head>
+<body>
+  <h1><?= $page->title()->esc() ?></h1>
+</body>
+</html>
